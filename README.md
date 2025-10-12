0b913d6f1d454dc89025bd3a9161892e144372a3
boli <bipinoli90@gmail.com>
2025-10-12 13:23:44 +0200

    correct handling of foregrounding and backgrounding of proccesses

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 - [X] Exiting shell
 - [X] Built in `cd` & `pwd`
 - [ ] Can run any programs in `/bin`
-- [ ] Spawning processes & waiting for execution
-- [ ] Handling `Ctrl + C` to kill a process
+- [X] Spawning processes & controlling them
+- [X] Handling `Ctrl + C` to kill a process
 - [ ] `Ctrl + Z` and `&` to run in the background 
-- [ ] `fg` to bring to foreground
+- [X] `fg` to bring to foreground
 - [ ] IO redicrection `>`, `>>`, `<`
 - [ ] Pipe `|`
 - [ ] Command history buffer (up/down arrow)
--- process.hpp
+++ process.hpp
@@ -9,59 +9,49 @@ namespace process {
   namespace __internal {
     pid_t bg_job;
 
-    void sigint_handler(int) {
-      cout << "Handling CTRL + Z" << endl;
+    void set_process_grp_to_fg(pid_t pid) {
+      // Note:
+      // `tcsetpgrp` can be called by any process in the terminal session
+      // However, within the terminal session there are foreground and bacckground proccess
+      // The `tcsetpgrp` can be called by the foreground proccess to set other process group into foreground
+      // Taking itself into background
+      // However, if the process is in background within the same terminal session
+      // it can still call `tcsetpgrp` and bring itself into foreground if the `SIGTTOU` signal is ingored within
+      //
+      // Therefore this function must only be called from the parent process
+      // To put child into foreground & later bring itself back into 
+      // foreground again to successfully get access to STDIN
+      //
+      // Also note that it is not necessary to set all STDIN, STDOUT, STDERR file descriptor to the process group to make foregound
+      // Doing any one is enough
+      if (tcsetpgrp(STDIN_FILENO, pid) == -1) {
+        cerr << "CRASH! tcsetpgrp() failed to assign process group to foreground. Error: " << errno << endl;
+        exit(1);
+      }
     }
   }
 
   void init() {
+    // ignore CTRL + C, CTRL + Z
     signal(SIGINT, SIG_IGN);
     signal(SIGTSTP, SIG_IGN);
+    // ingore bacgkround process (spawned child) trying to acccess stdin, stdout to write to terminal
+    // Also important to be able to set itself back to foreground in the terminal session
+    signal(SIGTTOU, SIG_IGN);
+    signal(SIGTTIN, SIG_IGN);
   }
 
   void spawn(const string& path, vector<string>& command) {
-    // create pipe for IPC to order parent event before child's
-    int pipe_fd[2];
-    if (pipe(pipe_fd) == -1) { // man 2 pipe
-      cerr << "CRASH! pipe() failed. Not possible to configure spawned process correctly!" << endl;
-      exit(1);
-    }
-    
-    auto pid = fork();
-    if (pid == -1) {
+    auto child_pid = fork();
+    if (child_pid == -1) {
       cerr << "CRASH! fork() failed" << endl;
       exit(1);
     }
-    if (pid == 0) {
-      // we want child to be 
+    if (child_pid == 0) {
       // SIG_IGN handlers are inherited in children
       // So set to default
       signal(SIGINT, SIG_DFL);
       signal(SIGTSTP, SIG_DFL);
-
-      // set child to be in the foreground group
-      // important because in macOS the ctrl + z 
-      // is sent to the whole process group
-      // but before that we must wait until the
-      // parent has set child to be in its own process group
-      //
-      // Note: The file descriptors are copid during fork()
-      // so we must close the unused descripter here so that 
-      // there are no duplicate producer/consumer on the pipe
-      // thus preventing the pipe close when closed from the 
-      // other process
-      close(pipe_fd[1]);
-      char dummy;
-      if (read(pipe_fd[0], &dummy, 1) == -1) { // man 2 read
-        cerr << "CRASH! failed to read from the IPC pipe" << endl;
-        exit(1);
-      }
-      close(pipe_fd[0]);
-      // setting foregroud proccess group id 
-      if (tcsetpgrp(STDIN_FILENO, getpgrp()) == -1) {
-        cerr << "CRASH! tcsetpgrp() failed to assign child into foreground" << endl;
-        exit(1);
-      } 
 
       vector<char*> argv;
       for (auto c: command) {
@@ -73,48 +63,14 @@ namespace process {
         exit(1);
       }
     } else {
-      // in MacOS the SIGTSTP signal is sent to the entire process group
+      // in MacOS the SIGTSTP (CTRL + Z) signal is sent to the entire process group
       // to avoid other processes including the parent receiving the signal
       // we should isolate the child process by making it its own process group leader
-      setpgid(pid, pid); // see `man 2 setpgid`
+      setpgid(child_pid, child_pid); // see `man 2 setpgid`
+      // And the child process group should be brought to the foreground
+      // such that the signals are received by only child 
+      __internal::set_process_grp_to_fg(child_pid);
 
-      // Note: The file descriptors are copid during fork()
-      // so we must close the unused descripter here so that 
-      // there are no duplicate producer/consumer on the pipe
-      // thus preventing the pipe close when closed from the 
-      // other process
-      close(pipe_fd[0]); // man 2 close
-      char dummy = '-';
-      if (write(pipe_fd[1], &dummy, 1) == -1) { // man 2 write
-        cerr << "CRASH! failed to write to the IPC pipe" << endl;
-        exit(1);
-      }
-      close(pipe_fd[1]);
-
-
-      int status;
-      if (waitpid(pid, &status, WUNTRACED) == -1) {
-        cerr << "CRASH! failed to wait for the completion of child process" << endl;
-        exit(1);
-      }
-      if (WIFSTOPPED(status)) {
-        if (WSTOPSIG(status) == SIGTSTP) {
-          cout << "Parent: child stopped with ctrl + z" << endl;
-          __internal::bg_job = pid;
-        }
-      }
-      if (WIFEXITED(status) || WIFSIGNALED(status)) {
-          cout << "Parent: child finished or killed" << endl;
-        __internal::bg_job = 0;
-      }
-    }
-  }
-
-  void bring2fg() {
-    auto child_pid = __internal::bg_job;
-    if (child_pid) {
-      kill(child_pid, SIGCONT);
-      __internal::bg_job = 0;
       int status;
       if (waitpid(child_pid, &status, WUNTRACED) == -1) {
         cerr << "CRASH! failed to wait for the completion of child process" << endl;
@@ -122,13 +78,39 @@ namespace process {
       }
       if (WIFSTOPPED(status)) {
         if (WSTOPSIG(status) == SIGTSTP) {
-          cout << "Parent: child stopped with ctrl + z" << endl;
+          __internal::set_process_grp_to_fg(getpgrp());
           __internal::bg_job = child_pid;
+          cout << "Process suspended to the background. Resume with `fg`" << endl;
         }
       }
       if (WIFEXITED(status) || WIFSIGNALED(status)) {
-          cout << "Parent: child finished or killed" << endl;
-        __internal::bg_job = 0;
+          __internal::set_process_grp_to_fg(getpgrp());
+          __internal::bg_job = 0;
+      }
+    }
+  }
+
+  void bring2fg() {
+    auto child_pid = __internal::bg_job;
+    if (child_pid) {
+      __internal::bg_job = 0;
+      __internal::set_process_grp_to_fg(child_pid);
+      kill(child_pid, SIGCONT);
+      int status;
+      if (waitpid(child_pid, &status, WUNTRACED) == -1) {
+        cerr << "CRASH! failed to wait for the completion of child process" << endl;
+        exit(1);
+      }
+      if (WIFSTOPPED(status)) {
+        if (WSTOPSIG(status) == SIGTSTP) {
+          __internal::set_process_grp_to_fg(getpgrp());
+          __internal::bg_job = child_pid;
+          cout << "Process suspended to the background. Resume with `fg`" << endl;
+        }
+      }
+      if (WIFEXITED(status) || WIFSIGNALED(status)) {
+          __internal::set_process_grp_to_fg(getpgrp());
+          __internal::bg_job = 0;
       }
     }
   }
